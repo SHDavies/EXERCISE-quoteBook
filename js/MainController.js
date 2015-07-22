fd47e1a6c39f771cab09c6ae9f033cf40f1e6a72
Spencer Davies <spencerhdavies@gmail.com>
2015-07-21 21:23:30 -0600

    finished project, added some sweet extra features

--- js/MainController.js
+++ js/MainController.js
@@ -1,4 +1,30 @@
-angular.module('quoteBook').controller('MainController', function($scope, MainService) {
+angular.module('quoteBook').controller('MainController', function($scope,MainService) {
 
-	
+	$scope.getQuotes = function() {
+		$scope.quotes = MainService.getData();
+	}();
+
+	$scope.buttonClick = function() {
+		if ($scope.radio === "add") {
+			$scope.quotes = MainService.addData($scope.textBox, $scope.authorBox);
+		} else if ($scope.radio === "filter") {
+			$scope.quoteFilter = $scope.textBox;
+			$scope.showFilterButton = true;
+		}
+	};
+
+	$scope.showFilterButton = false;
+
+	$scope.removeFilter = function() {
+		$scope.quoteFilter = "";
+		$scope.showFilterButton = false;
+	};
+
+	$scope.resetQuotes = function() {
+		$scope.quotes = MainService.resetStorage();
+	};
+
+	$scope.removeButton = function(toRemove) {
+		$scope.quotes = MainService.removeData(toRemove);
+	};
 })
--- js/MainService.js
+++ js/MainService.js
@@ -1,4 +1,57 @@
 angular.module('quoteBook').service('MainService', function() {
 
-	
+	var quotes = [
+    { text: 'Life isn\'t about getting and having, it\'s about giving and being.', author: 'Kevin Kruse'},
+    { text: 'Whatever the mind of man can conceive and believe, it can achieve', author: 'Napoleon Hill'},
+    { text: 'Strive not to be a success, but rather to be of value.', author: 'Albert Einstein'},
+    { text: 'Two roads diverged in a wood, and I took the one less traveled by, And that has made all the difference.', author: 'Robert Frost'},
+    { text: 'The most difficult thing is the decision to act, the rest is merely tenacity.', author: 'Amelia Earhart'},
+    { text: 'Life is what happens to you while you\'re busy making other plans.', author: 'John Lennon'},
+    { text: 'What even is a jQuery?', author: 'Tyler S. McGinnis'}
+  ];
+
+  if (localStorage.length === 0) {
+  	localStorage.setItem('quoteStorage', JSON.stringify(quotes));
+  }
+
+  var storedQuotes = JSON.parse(localStorage.getItem('quoteStorage'));
+
+  this.getData = function() {
+  	return storedQuotes;
+  };
+
+  this.addData = function(newText, authorName) {
+  	var newQuote = {
+  		text: newText,
+  		author: authorName
+  	};
+  	quotes.push(newQuote);
+  	localStorage.setItem('quoteStorage', JSON.stringify(quotes));
+  	return quotes;
+  };
+
+  this.removeData = function(toRemove) {
+  	for (var i = 0; i < quotes.length; i++) {
+  		if (quotes[i].text === toRemove) {
+  			quotes.splice(i, 1);
+  		}
+  	}
+  	localStorage.setItem('quoteStorage', JSON.stringify(quotes));
+  	return quotes;
+  };
+
+  this.resetStorage = function() {
+  	localStorage.clear();
+  	localStorage.setItem('quoteStorage', JSON.stringify(quotes));
+  	quotes = [
+    { text: 'Life isn\'t about getting and having, it\'s about giving and being.', author: 'Kevin Kruse'},
+    { text: 'Whatever the mind of man can conceive and believe, it can achieve', author: 'Napoleon Hill'},
+    { text: 'Strive not to be a success, but rather to be of value.', author: 'Albert Einstein'},
+    { text: 'Two roads diverged in a wood, and I took the one less traveled by, And that has made all the difference.', author: 'Robert Frost'},
+    { text: 'The most difficult thing is the decision to act, the rest is merely tenacity.', author: 'Amelia Earhart'},
+    { text: 'Life is what happens to you while you\'re busy making other plans.', author: 'John Lennon'},
+    { text: 'What even is a jQuery?', author: 'Tyler S. McGinnis'}
+  	];
+  	return quotes;
+  }
 })
